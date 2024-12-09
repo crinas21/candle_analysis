7d5e1adab2103ba9a0f29738bf98a94cfbff5bbc
Cohen Rinas <crin9646@uni.sydney.edu.au>
2024-12-09 22:53:26 +1100

    Attempting Vercel Deployment 4

--- candles/build_files.sh
+++ candles/build_files.sh
@@ -2,5 +2,5 @@
 
 pip install -r requirements.txt
 python3 manage.py makemigrations --noinput
-mython3 manage.py migrate --noinput
+python3 manage.py migrate --noinput
 python3 manage.py collectstatic --noinput
