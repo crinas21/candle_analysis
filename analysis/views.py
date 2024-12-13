8d5c4eedc668e466d1de840d2a13657e234a54a7
Cohen Rinas <crin9646@uni.sydney.edu.au>
2024-12-13 18:00:42 +1100

    Added pagination and check for API Call limit eing reached

--- analysis/views.py
+++ analysis/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.core.files.storage import FileSystemStorage
 from .forms import CustomUserCreationForm  # Import the custom form
 
@@ -39,16 +40,40 @@ def matches(request):
     else:
         return JsonResponse({'error': 'Invalid request'}, status=400)
 
-    
+
 def analysis(request):
     if request.method == 'GET':
         symbol = request.GET.get('symbol', '').strip()
-        days = 100 # TODO: Get from user input
-        alpha_data = utils.get_alpha_data(symbol, days)
-        pattern_data = utils.get_pattern_data(alpha_data)
-        chart_html = utils.get_chart_html(pattern_data)
+        days = 100  # TODO: Get from user input
         
-        return render(request, 'analysis.html', {'symbol': symbol, 'table_data': pattern_data, 'chart_html': chart_html})
+        # Get rows_per_page from user input, default to 10 if not provided or invalid
+        try:
+            rows_per_page = int(request.GET.get('rows_per_page', 10))
+            rows_per_page = max(rows_per_page, 10)  # Ensure a minimum of 10 rows
+        except ValueError:
+            rows_per_page = 10  # Default to 10 if input is invalid
+
+        page_number = int(request.GET.get('page', 1))  # Get the current page number
+
+        alpha_data = utils.get_alpha_data(symbol, days)
+        if alpha_data == {}:
+            return render(request, 'error.html')
+
+        pattern_data = utils.get_pattern_data(alpha_data)
+
+        # Paginate the data
+        paginator = Paginator(pattern_data, rows_per_page)
+        current_page_data = paginator.get_page(page_number)
+
+        chart_html = utils.get_chart_html(pattern_data)
+
+        return render(request, 'analysis.html', {
+            'symbol': symbol,
+            'table_data': current_page_data,
+            'chart_html': chart_html,
+            'paginator': paginator,
+            'rows_per_page': rows_per_page,
+        })
     else:
         return JsonResponse({'error': 'Invalid request'}, status=400)
     
